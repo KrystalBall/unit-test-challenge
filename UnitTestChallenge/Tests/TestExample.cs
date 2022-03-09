88c39c3e002c0fe1cf3eb56cdd91c927fb29a282
that1chris <101151465+that1chris@users.noreply.github.com>
2022-03-09 16:58:40 -0500

    Initial tests

--- UnitTestChallenge/Tests/TestExample.cs
+++ UnitTestChallenge/Tests/TestExample.cs
@@ -29,32 +29,204 @@ public class TestExample
     }
 
     [TestMethod]
-    public void ShouldGetOrganizationUsers()
+    public void ShouldErrorIfNoSpecifiedUser()
     {
-        //Use Fixture to create a list of users in many organizations 
-        //Then call OrganizationUserHelper.GetUsers(userList, organizationID)
-        //Test needs to verify that only users in the specified organization are returned
+        Fixture fixture = new();
+        var organization = fixture.Build<Organization>().With(o => o.OrganizationID, 1).Create();
+        var organizationUsers = fixture
+            .Build<User>()
+            .With(o => o.Organization, organization)
+            .CreateMany()
+            .ToList();
+
+        var userToFind = organizationUsers.First();
+
+        UserList userList = new() { Users = organizationUsers };
+
+        // Let those silly humans care about the error message. Just check the type so that the test is less brittle.
+        Assert.ThrowsException<Exception>(() => SingleUserHelper.GetUser(userList, 2));
     }
 
     [TestMethod]
-    public void ShouldGetRoleUsers()
+    public void ShouldGetCorrectOrganizationUser()
     {
-        //Use Fixture to create a list of users with different roles 
-        //Then call RoleHelper.GetUsers(userList, roleID)
-        //Test needs to verify that only users in the specified role are returned
+        // I decided to double check AutoFixture's trustworthiness for random, unique IDs between multiple instances of complex objects.
+        // The fruits of my labor bear a comment. Huzzah!
+        // A good starting point for code-gazing for the curious: https://github.com/AutoFixture/AutoFixture/blob/730b47884975d0c1256209073a719a9fe510d8a9/Src/AutoFixture/RandomNumericSequenceGenerator.cs#L142
+        
+        Fixture fixture = new();
+        var organizationUsers = fixture
+            .Build<User>()
+            .CreateMany(2)
+            .ToList();
+
+        var expectedId = organizationUsers[0].Organization.OrganizationID;
+
+        UserList userList = new() { Users = organizationUsers };
+
+        var returnedUsers = OrganizationUserHelper.GetUsers(userList, expectedId);
+
+        Assert.AreEqual(expectedId, returnedUsers.Users[0].Organization.OrganizationID);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(GetUserCountTestData), DynamicDataSourceType.Method)]
+    public void ShouldGetAllValidOrganizationUsers(List<User> organizationUsers, int expectedCount)
+    {
+        UserList userList = new() { Users = organizationUsers };
+
+        var returnedUsers = OrganizationUserHelper.GetUsers(userList, organizationUsers[0].Organization.OrganizationID);
+
+        // TotalCount being accurate is a dependency. Looking at the list directly is arguably more reliable.
+        Assert.AreEqual(expectedCount, returnedUsers.Users.Count);
     }
 
     [TestMethod]
-    public void ShouldGetOrganizationRoleUsers()
+    public void ShouldGetNoOrganizationUserIfNoMatch()
     {
-        //Use Fixture to create a list of users with the following specs
-        //  many organizations
-        //  many roles
-        //  role ids must be the same across organizations 
-        //Then call RoleHelper.GetUsers(userList, roleID)
-        //Test needs to verify that only users in the specified role and organization are returned
+        Fixture fixture = new();
+        var organization = fixture.Build<Organization>().With(o => o.OrganizationID, 1).Create();
+        var organizationUsers = fixture
+            .Build<User>()
+            .With(o => o.Organization, organization)
+            .CreateMany()
+            .ToList();
+
+        UserList userList = new() { Users = organizationUsers };
+
+        var returnedUsers = OrganizationUserHelper.GetUsers(userList, 2);
+
+        Assert.AreEqual(0, returnedUsers.Users.Count);
     }
 
-    //Based on your experience and analysis of the models, are there any other tests you feel would be useful?
-    //If so create them here.
+    [TestMethod]
+    public void ShouldGetCorrectRoleUsers()
+    {
+        Fixture fixture = new();
+        var roleUsers = fixture
+            .Build<User>()
+            .CreateMany(2)
+            .ToList();
+
+        var expectedId = roleUsers[0].SystemRole.SystemRoleID;
+
+        UserList userList = new() { Users = roleUsers };
+
+        var returnedUsers = RoleHelper.GetUsers(userList, expectedId);
+
+        Assert.AreEqual(expectedId, returnedUsers.Users[0].SystemRole.SystemRoleID);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(GetUserCountTestData), DynamicDataSourceType.Method)]
+    public void ShouldGetAllValidRoleUsers(List<User> roleUsers, int expectedCount)
+    {
+        UserList userList = new() { Users = roleUsers };
+
+        var returnedUsers = RoleHelper.GetUsers(userList, roleUsers[0].SystemRole.SystemRoleID);
+
+        Assert.AreEqual(expectedCount, returnedUsers.Users.Count);
+    }
+
+    [TestMethod]
+    public void ShouldGetNoRoleUserIfNoMatch()
+    {
+        Fixture fixture = new();
+        var role = fixture.Build<SystemRole>().With(r => r.SystemRoleID, 1).Create();
+        var roleUsers = fixture
+            .Build<User>()
+            .With(r => r.SystemRole, role)
+            .CreateMany()
+            .ToList();
+
+        UserList userList = new() { Users = roleUsers };
+
+        var returnedUsers = RoleHelper.GetUsers(userList, 2);
+
+        Assert.AreEqual(0, returnedUsers.Users.Count);
+    }
+
+    [TestMethod]
+    public void ShouldGetCorrectOrganizationRoleUsers()
+    {
+        Fixture fixture = new();
+        var users = fixture
+            .Build<User>()
+            .CreateMany(2)
+            .ToList();
+
+        var expectedRoleId = users[0].SystemRole.SystemRoleID;
+        var expectedOrganizationId = users[0].Organization.OrganizationID;
+
+        UserList userList = new() { Users = users };
+
+        var returnedUsers = RoleAndOrganizationHelper.GetUsers(userList, expectedOrganizationId, expectedRoleId);
+
+        // Multiple asserts in one test usually isn't ideal, but I think a test like this calls for an exception.
+        Assert.AreEqual(expectedRoleId, returnedUsers.Users[0].SystemRole.SystemRoleID);
+        Assert.AreEqual(expectedOrganizationId, returnedUsers.Users[0].Organization.OrganizationID);
+    }
+
+    [DataTestMethod]
+    [DynamicData(nameof(GetUserCountTestData), DynamicDataSourceType.Method)]
+    public void ShouldGetAllValidOrganizationRoleUsers(List<User> users, int expectedCount)
+    {
+        UserList userList = new() { Users = users };
+
+        var returnedUsers = RoleAndOrganizationHelper.GetUsers(userList, users[0].Organization.OrganizationID, users[0].SystemRole.SystemRoleID);
+
+        Assert.AreEqual(expectedCount, returnedUsers.Users.Count);
+    }
+
+    [TestMethod]
+    public void ShouldGetNoOrganizationRoleUsersIfNoExactMatch()
+    {
+        Fixture fixture = new();
+        var users = fixture
+            .Build<User>()
+            .CreateMany(2)
+            .ToList();
+
+        var expectedRoleId = users[0].SystemRole.SystemRoleID;
+        var expectedOrganizationId = users[1].Organization.OrganizationID;
+
+        UserList userList = new() { Users = users };
+
+        var returnedUsers = RoleAndOrganizationHelper.GetUsers(userList, expectedOrganizationId, expectedRoleId);
+
+        Assert.AreEqual(0, returnedUsers.Users.Count);
+    }
+
+    private static IEnumerable<object[]> GetUserCountTestData()
+    {
+        var fixture = new Fixture();
+        var organization = fixture
+            .Build<Organization>()
+            .With(o => o.OrganizationID, 1)
+            .Create();
+        var role = fixture
+            .Build<SystemRole>()
+            .With(r => r.SystemRoleID, 1)
+            .Create();
+
+        yield return new object[] 
+        {
+            fixture
+                .Build<User>()
+                .With(o => o.Organization, organization)
+                .With(r => r.SystemRole, role)
+                .CreateMany(3)
+                .ToList(), 
+            3 
+        };
+
+        yield return new object[] 
+        {
+            fixture
+                .Build<User>()
+                .CreateMany(2)
+                .ToList(), 
+            1 
+        };
+    }
 }
